75aadb319adeea4324fdc7a0ea9940eca338f9f0
Alex Montoya <montoyao@hawaii.edu>
2021-05-18 19:50:07 -1000

    added documentation to describe code blocks.

---
+++ testcpu/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenHardwareMonitor.Hardware;
+using System.IO.Ports;
+
+namespace Get_CPU_Temp5
+{
+    class Program
+    {
+        public class UpdateVisitor : IVisitor
+        {
+            public void VisitComputer(IComputer computer)
+            {
+                computer.Traverse(this);
+            }
+            public void VisitHardware(IHardware hardware)
+            {
+                hardware.Update();
+                foreach (IHardware subHardware in hardware.SubHardware) subHardware.Accept(this);
+            }
+            public void VisitSensor(ISensor sensor) { }
+            public void VisitParameter(IParameter parameter) { }
+        }
+        static void GetSystemInfo()
+        {
+            UpdateVisitor updateVisitor = new UpdateVisitor();
+            Computer computer = new Computer();
+            computer.Open();
+            computer.CPUEnabled = true;
+            computer.GPUEnabled = true;
+            computer.HDDEnabled = true;
+            computer.RAMEnabled = true;
+            computer.FanControllerEnabled = true;
+            computer.MainboardEnabled = true;
+            computer.Accept(updateVisitor);
+
+
+            //for (int i = 0; i < computer.Hardware.Length; i++)
+            //{
+            //    if (computer.Hardware[i].HardwareType == HardwareType.CPU)
+            //    {
+            //        for (int j = 0; j < computer.Hardware[i].Sensors.Length; j++)
+            //        {
+            //            if (computer.Hardware[i].Sensors[j].SensorType == SensorType.Temperature)
+            //                Console.WriteLine(computer.Hardware[i].Sensors[j].Name + ":" + computer.Hardware[i].Sensors[j].Value.ToString() + "\r");
+            //        }
+            //    }
+            //}
+
+            for (int i = 0; i < computer.Hardware.Length; i++)
+            {
+                switch (computer.Hardware[i].HardwareType)
+                {
+                    case HardwareType.CPU:
+                        Console.Write("CPU: ");
+                        break;
+                    case HardwareType.GpuNvidia:
+                        Console.Write("GPU: ");
+                        break;
+                    case HardwareType.HDD:
+                        Console.Write("HDD: ");
+                        break;
+                    case HardwareType.RAM:
+                        Console.Write("RAM: ");
+                        break;
+                    case HardwareType.Mainboard:
+                        Console.Write("Mainboard: ");
+                        break;
+                    default:
+                        Console.Write("???: ");
+                        break;
+                }
+
+                if(computer.Hardware[i].HardwareType == HardwareType.CPU)
+                {
+                    SerialPort arduino = new SerialPort("COM4", 9600);
+                    arduino.Open();
+                    arduino.Write(computer.Hardware[i].Name);
+                    arduino.Close();
+                }
+
+                Console.WriteLine(computer.Hardware[i].Name);
+
+                for (int j = 0; j < computer.Hardware[i].Sensors.Length; j++)
+                {
+                    switch (computer.Hardware[i].Sensors[j].SensorType)
+                    {
+                        case SensorType.Clock:
+                            Console.Write("(clock)");
+                            break;
+                        case SensorType.Control:
+                            Console.Write("(control)");
+                            break;
+                        case SensorType.Data:
+                            Console.Write("(data)");
+                            break;
+                        case SensorType.Factor:
+                            Console.Write("(factor)");
+                            break;
+                        case SensorType.Fan:
+                            Console.Write("(fan)");
+                            break;
+                        case SensorType.Flow:
+                            Console.Write("(flow)");
+                            break;
+                        case SensorType.Level:
+                            Console.Write("(level)");
+                            break;
+                        case SensorType.Load:
+                            Console.Write("(load)");
+                            break;
+                        case SensorType.Power:
+                            Console.Write("(power)");
+                            break;
+                        case SensorType.SmallData:
+                            Console.Write("(smalldata)");
+                            break;
+                        case SensorType.Temperature:
+                            Console.Write("(temp)");
+                            break;
+                        case SensorType.Throughput:
+                            Console.Write("(throughput)");
+                            break;
+                        case SensorType.Voltage:
+                            Console.Write("(voltage)");
+                            break;
+                        default:
+                            break;
+                    }    
+                    Console.WriteLine(computer.Hardware[i].Sensors[j].Name + ": " + computer.Hardware[i].Sensors[j].Value.ToString() + "\t");
+                }
+                Console.WriteLine(" ");
+            }
+            computer.Close();
+        }
+        static void Main(string[] args)
+        {
+            //SerialPort arduino = new SerialPort("COM4", 9600);
+            //arduino.Open();
+
+            //while (true)
+            //{
+            //    Thread.Sleep(500);
+            //    Console.Clear();
+            //    GetSystemInfo();
+
+            //}
+            GetSystemInfo();
+            
+            //arduino.Write("test test test");
+            //arduino.Close();
+
+
+
+        }
+    }
+}
